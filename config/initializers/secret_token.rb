c52321283dc3c691f74f9215bab5b2e8de40f4f3
Ben Andrews <ben.andrews@gmail.com>
2011-08-04 21:41:19 -0700

    First commit - Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StrataERP::Application.config.secret_token = 'fcadea3e2f2aa0515712e84eca7495bf15832e4870165b17bfbbdbdb340252c67eda5ee47f895f243ce498ea6a21327c213e25400063f316445f8f428863d540'
